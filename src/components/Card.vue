794ea07c10f8f425f1057d9e20ccffacd6db5d7a
Onur KINAY <ben@onurkinay.com.tr>
2025-08-25 17:21:02 +0300

    homecard sand card

---
+++ src/components/Card.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { defineProps } from 'vue';
+defineProps({
+    bg:String,
+    default: 'bg-gray-100'
+})
+</script>
+
+<template>
+  <div :class="`${type} p-6 rounded-lg shadow-md `">
+    <slot></slot>
+  </div>
+</template>
