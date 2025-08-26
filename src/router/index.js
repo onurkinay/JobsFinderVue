461a7646eac586030689f4851fba9b2afcd5be16
Onur KINAY <ben@onurkinay.com.tr>
2025-08-26 09:53:23 +0300

    routerlink

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,16 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
-
+import JobsView from "@/views/JobsView.vue";
 const routes = [
   {
     path: "/",
     name: "Home",
     component: HomeView,
+  },
+  {
+    path: "/jobs",
+    name: "Jobs",
+    component: JobsView,
   },
 ];
 
