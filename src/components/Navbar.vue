cbaee59c7cd368ff6e09a19e462e7ea9f43341d7
Onur KINAY <ben@onurkinay.com.tr>
2025-08-26 10:00:01 +0300

    active link

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,11 @@
 <script setup>
 import logo from "@/assets/img/logo.png";
-import { RouterLink } from "vue-router";
+import { RouterLink, useRoute } from "vue-router";
+
+const isActiveLink = (route) => {
+  const currentRoute = useRoute();
+  return currentRoute.path === route;
+};
 </script>
 
 <template>
@@ -21,17 +26,41 @@ import { RouterLink } from "vue-router";
             <div class="flex space-x-2">
               <RouterLink
                 to="/"
-                class="text-white bg-green-900 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                :class="[
+                  isActiveLink('/') ? 'bg-green-900' : '',
+                  'text-white',
+                  'hover:bg-gray-900',
+                  'hover:text-white',
+                  'rounded-md',
+                  'px-3',
+                  'py-2',
+                ]"
                 >Home</RouterLink
               >
               <RouterLink
                 to="/jobs"
-                class="text-white hover:bg-green-900 hover:text-white rounded-md px-3 py-2"
+                :class="[
+                  isActiveLink('/jobs') ? 'bg-green-900' : '',
+                  'text-white',
+                  'hover:bg-gray-900',
+                  'hover:text-white',
+                  'rounded-md',
+                  'px-3',
+                  'py-2',
+                ]"
                 >Jobs</RouterLink
               >
               <RouterLink
                 to="/jobs/add"
-                class="text-white hover:bg-green-900 hover:text-white rounded-md px-3 py-2"
+                :class="[
+                  isActiveLink('/jobs/add') ? 'bg-green-900' : '',
+                  'text-white',
+                  'hover:bg-gray-900',
+                  'hover:text-white',
+                  'rounded-md',
+                  'px-3',
+                  'py-2',
+                ]"
                 >Add Job</RouterLink
               >
             </div>
